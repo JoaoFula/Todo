6a3f0aeb0bb2e56782f6a9ac70166a7aaa8cfe10
JoaoFula <hsifula@gmail.com>
2020-09-07 18:11:35 +0100

    Revert "Confusion should be solved"
    This reverts commit 0c902e23988bcd0da23ed66ac946def9b7d90ac4.

--- todo/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import todo
-
-class todo_admin(admin.ModelAdmin):
-    readonly_fields = ('created',)
-
-# Register your models here.
-admin.site.register(todo, todo_admin)
--- todo/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import todo
-
-class todo_form(ModelForm):
-    class Meta:
-        model = todo
-        fields = ['title', 'memo', 'important']
